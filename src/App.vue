5dbac5cec02c891968d2888b6dd9d6010a3e5785
carlos altamirano <carlos0altamirano@gmail.com>
2021-08-10 13:01:27 -0500

    load files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Rick and Morty</h1>
+    <list-characters />
+  </div>
+</template>
+
+<script>
+import ListCharacters from "@/components/ListCharacters";
+export default {
+  name: "App",
+  components: {
+    ListCharacters,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/CardCharacter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="character">
+    <div class="character__info">
+      {{ character.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["character"]
+};
+</script>
+
+<style>
+</style>
